a1ef195a180760a60e63465eed2af8da80396136
VadimS4 <vstakhnyuk@outlook.com>
2020-09-22 08:41:44 -0700

    Created a responsive chart.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { LineChart, Line, XAxis, Tooltip} from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer} from 'recharts';
 import '../styles/Chart.css';
 
 class Chart extends React.Component {
     render() {
         return (
             <div className="chart">
-                <span>Retail Sales</span>
-                <LineChart width={1080} height={450} data={this.props.data.sales}>
-                    <XAxis dataKey="weekEnding"/>
-                    <Tooltip />
-                    <Line type="monotone" dataKey="retailSales" stroke="#40a8ef" />
-                    <Line type="monotone" dataKey="wholesaleSales" stroke="#3c4858" />
-                </LineChart>
+                <span className="chart-title">Retail Sales</span>
+                <ResponsiveContainer width="100%" height="50%" className="resp-container">
+                    <LineChart data={this.props.data.sales}>
+                        <XAxis dataKey="weekEnding"/>
+                        <Tooltip />
+                        <Line type="monotone" dataKey="retailSales" stroke="#40a8ef" strokeWidth={2} />
+                        <Line type="monotone" dataKey="wholesaleSales" stroke="#3c4858" strokeWidth={2} />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         )
     }
